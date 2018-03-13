fb6825c51d417e0f6eb739694531ee2b7489e828
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 12:44:55 +0200

    Update cpu_class.py

--- cpu_class.py
+++ cpu_class.py
@@ -1,20 +1,21 @@
 import psutil
 
+
 class CpuAnalazer:
-    #
+
     def cpuIntervalTimeAnalyze(self):
         for x in range(1):
-            return psutil.cpu_percent(interval = 5)
-    #
+            return psutil.cpu_percent(interval=5)
+
     def cpuIntervalTimePerCpuAnalyze(self):
         for x in range(1):
-            return psutil.cpu_times_percent(interval = 5, percpu = False)
-    #
+            return psutil.cpu_times_percent(interval=5, percpu=False)
+
     def cpuCount(self):
         return psutil.cpu_count(self)
-    #
+
     def cpuCountLogical(self):
-        return psutil.cpu_count(logical = False)
-    #
+        return psutil.cpu_count(logical=False)
+
     def cpuStat(self):
         return psutil.cpu_stats()
