546ba76eceed1f5e0f6be0336282fc532eaf6b6b
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 13:02:51 +0200

    Update test_main_class.py

--- test_main_class.py
+++ test_main_class.py
@@ -1,10 +1,11 @@
 from main_class import *
 import unittest
 
+
 class TestSequence(unittest.TestCase):
     def testsample(self):
-      ape = MonitorPyEnvJson()
-      self.assertFalse(ape.outResultSetting())
+        ape = MonitorPyEnvJson()
+        self.assertFalse(ape.outResultSetting())
 
 if __name__ == '__main__':
     unittest.main()
