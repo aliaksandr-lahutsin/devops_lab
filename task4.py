df7efb6860f36a7b30f318feed93e0638c0003cd
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 15:15:06 +0200

    Update task4.py

--- task4.py
+++ task4.py
@@ -15,11 +15,10 @@ for age, gender in dictionary_list.items():
     if (gender == 1):
         men_dictionary_list.append(age)
     m_count += 1
-    else:
-        if (m_count == 0):
-            print('-1')
+    if (m_count == 0):
+        print('-1')
 m = max(men_dictionary_list)
 counter = [i for i in range(
-       len(men_dictionary_list)) if (men_dictionary_list[i] == m]
-       ):
-    print(counter)
+    len(men_dictionary_list)) 
+    if men_dictionary_list[i] == m] 
+print(counter)
