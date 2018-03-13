60ef619eb899377c252d3781844004795c337270
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 12:42:37 +0200

    Update ram_class.py

--- ram_class.py
+++ ram_class.py
@@ -1,9 +1,10 @@
 import psutil
 
+
 class RamAnalazer:
-    #
+
     def virualMemoryUsage(self):
         return psutil.virtual_memory()
-    #
+
     def swapMemoryUsage(self):
-        return psutil.swap_memory()  
+        return psutil.swap_memory() 
