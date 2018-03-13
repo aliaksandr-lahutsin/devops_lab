bce81a21a73d4c1753928d6538e41eb3da9a6cb4
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 15:26:33 +0200

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,22 +1,6 @@
-def count_repeat_symbols(string):
-    char_list = list(string)
-    char_set = set(char_list)
-    char_count = list()
-    for char in char_set:
-        char_count.append(string.count(char)) 
-    char_count.sort()
-    char_count.reverse()
-    char_count = char_count[0: 3] 
-    char_new_list = list()
-    for char in char_set:
-        if char_list.count(char) in char_count:
-            char_new_list.append(char)
-    char_new_list.sort()
-    for char in char_new_list:
-        print (char, char_list.count(char))
+from collections import Counter
 
-string = input()
-print(count_repeat_symbols(string))
+chars = Counter(input()).items()
 
-
-
+for char, n in sorted(chars, key=lambda c: (-c[1], c[0]))[:3]:
+    print(char, n)
