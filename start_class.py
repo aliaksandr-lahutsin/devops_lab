3809175de2aaed9c9de7f90835d0e8cb6ba9aa7e
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 14:38:34 +0200

    Update start_class.py

--- start_class.py
+++ start_class.py
@@ -1,4 +1,14 @@
 from super_class import *
+import json
+from apscheduler.scheduler import Scheduler
 
-ifs = MonitorJson()
-ifs.outResultSetting()
+sched = Scheduler()
+sched.start()
+
+
+def some_job():
+    ifs = MonitorJson()
+    ifs.outResultSetting()
+
+data = json.load(open('config.json'))
+sched.add_interval_job(some_job, seconds=data["interval"])
