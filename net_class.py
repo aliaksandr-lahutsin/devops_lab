950d25e707e1f7721cdf2fca5555a1f474df9d2d
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 12:43:09 +0200

    Update net_class.py

--- net_class.py
+++ net_class.py
@@ -1,12 +1,13 @@
 import psutil
 
+
 class NetAnalazer:
-    #    
+   
     def netConnectors(self):
         return psutil.net_connections()
-    #
+
     def netIfAddres(self):
         return psutil.net_if_addrs()  
-    #
+
     def netIfStats(self):
         return psutil.net_if_stats() 
