cb6b6999ef66502269c7be406d34e585dc809f27
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 12:28:45 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,32 +1,36 @@
-def accountment(a,b):
+def accountment(a, b):
     if '-' in a or '-' in b:
-      a = a.replace('-', '')
-      b = b.replace('-', '')
-      #
-      new_a = make_max_number(a)
-      new_b = make_max_number(b)
-      #
-      if (int(new_a) + int(new_b) > int(new_b) + int(new_a)):
+        a = a.replace('-', '')
+    b = b.replace('-', '')
+
+    new_a = make_max_number(a)
+    new_b = make_max_number(b)
+    
+    if (int(new_a) + int(new_b) > int(new_b) + int(new_a)):
         return int(new_a) + int(new_b)
-      else: return int(new_b) + int(new_a)
+    else: 
+        return int(new_b) + int(new_a)
+    
     else:
-      #
-      new_a = make_max_number(a)
-      new_b = make_max_number(b)
-      #
-      if (int(new_a) - int(new_b) > int(new_b) - int(new_a)):
-        return int(new_a) - int(new_b)
-      else: return int(new_b) - int(new_a)
-      #
-      print(int(a), int(b))
+
+        new_a = make_max_number(a)
+        new_b = make_max_number(b)
+
+        if (int(new_a) - int(new_b) > int(new_b) - int(new_a)):
+            return int(new_a) - int(new_b)
+        else: 
+            return int(new_b) - int(new_a)
+
+print(int(a), int(b))
+
 
 def make_max_number(number):
-  temp_number = []
-  for symbol in number:
+    temp_number = []
+for symbol in number:
     temp_number += symbol
     temp_number.sort(reverse=True)
-  return ''.join(temp_number)
-  
+return ''.join(temp_number)
+    
 a = input()
 b = input()
-print(accountment(a,b))
+print(accountment(a, b))
