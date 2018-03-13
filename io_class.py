dabbda625df1be3d2db124b55c2edf4960ab9e25
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 12:43:45 +0200

    Update io_class.py

--- io_class.py
+++ io_class.py
@@ -1,15 +1,16 @@
 import psutil
 
+
 class IOAnalazer:
-    #
+
     def diskPartitionUsage(self):
         return psutil.disk_partitions()
-    #
+
     def diskUsageUsage(self):
         return psutil.disk_usage('/')  
-    #
+
     def diskIOCounters(self):
-        return psutil.disk_io_counters(perdisk = True)    
-    #
+        return psutil.disk_io_counters(perdisk=True)    
+
     def netIOCounters(self):
-        return psutil.net_io_counters(pernic = True)   
+        return psutil.net_io_counters(pernic=True)   
