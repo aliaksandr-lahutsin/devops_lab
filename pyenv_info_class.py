2b97fc8b60c0ebf13ed3facaa5bbe42ae0137806
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-13 16:10:16 +0200

    Update pyenv_info_class.py
    The main thing in life so I didn't hit the fifth part of the body.

--- pyenv_info_class.py
+++ pyenv_info_class.py
@@ -5,14 +5,15 @@ import subprocess
 
 
 class AboutPythonEnvironment:
-       
+    
     def getSystemVersion(self):
         return sys.version
-        
+    
     def getVirtualEnvironment(self):
-        result = subprocess.run(
-            ['bash', '-c', 'pyenv virtualenvs'], 
-            stdout=subprocess.PIPE)
+        result = subprocess.run([
+            'bash',
+            '-c', 
+            'pyenv virtualenvs'], stdout=subprocess.PIPE)
         return result.stdout
     
     def getExecutableLocation(self):
@@ -23,15 +24,15 @@ class AboutPythonEnvironment:
     
     def getPythonPath(self):
         return sys.path
-
+    
     def getInstalledPackages(self):
         installed_packages = pip.get_installed_distributions()
         installed_packages_list = sorted(
             ["%s==%s" % (i.key, i.version)
-                for i in installed_packages])
-    return installed_packages_list
+                for i in installed_packages]
+        )
+        return installed_packages_list
     
     def getSitePackagesLocation(self):
         site_packages = next(p for p in sys.path if 'site-packages' in p)
         return site_packages
-    
